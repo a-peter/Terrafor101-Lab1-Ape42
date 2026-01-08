1b9a4f3a472b32cd56476b70dbae9c0aca66f6e5
Anselm Peter <anselm.peter@iav.de>
2026-01-08 16:00:52 +0100

    Added sample sensitive data

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,7 @@ output "environment_prefix" {
 output "suffix" {
   value = random_string.suffix.result
 }
+output "api_key" {
+  value = "${var.api_key}"
+  sensitive = true
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,7 @@ variable "application_name" {
 
 variable "environment_name" {
 }
+
+variable "api_key" {
+  sensitive = true
+}
