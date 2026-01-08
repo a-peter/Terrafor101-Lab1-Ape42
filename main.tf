4c94ad96447f56f456bec57705d0917ad8be6065
Anselm Peter <anselm.peter@iav.de>
2026-01-08 15:18:03 +0100

    Added suffix to environment

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "random_string" "suffix" {
 }
 
 locals {
-  environment_prefix = "${var.application_name}-${var.environment_name}"
+  environment_prefix = "${var.application_name}-${var.environment_name}-${random_string.suffix.result}"
 }
+
 
