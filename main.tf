fb4e83d60addfdb437510242080a8565686f0e27
Anselm Peter <anselm.peter@iav.de>
2026-01-14 13:21:43 +0100

    Lists and Count

--- main.tf
+++ main.tf
@@ -12,10 +12,17 @@ resource "random_string" "suffix" {
 }
 
 /*
-  prefix for the environment
+  prefix for the environment based on application name and environment name.
+  This is used to create a unique identifier for resources in different environments.
 */
 locals {
   environment_prefix = "${var.application_name}-${var.environment_name}-${random_string.suffix.result}"
 }
 
+resource "random_string" "list" {
+  count = length(var.regions)
 
+  length = 6
+  upper = false
+  special = false 
+}
